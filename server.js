a25987c3e6d5e0bdbd6ae2f7f4d7d6c1f7116ff0
Pan fried denzel <toubieogunleye@live.com>
2017-04-08 14:26:01 +0100

    Update server.js

--- server.js
+++ server.js
@@ -48,7 +48,6 @@ app.get("/:id", function(request, response) {
     response.send(val)
 });
 
-var listener = app.listen(3000,
-    function() {
+var listener = app.listen(process.env.PORT, function () {  
         console.log('Your app is listening on port ' + listener.address().port);
     });
